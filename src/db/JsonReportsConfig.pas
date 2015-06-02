f8944a1fde2d8915d028113415a7d140bc6f527d
Andris Klaipins <andris.klaipins@tieto.com>
2015-06-03 00:34:37 +0300

    cleanup for TJsonReportsConfig

--- src/db/JsonReportsConfig.pas
+++ src/db/JsonReportsConfig.pas
@@ -3,10 +3,30 @@ unit JsonReportsConfig;
 interface
 
 uses
-  Classes, System.JSON, System.IOUtils, Spring.Collections, ReportsConfig, ReportTemplate;
+  Classes, System.SysUtils, System.JSON, System.IOUtils, Spring.Collections, ReportsConfig, ReportTemplate;
 
 type
+  TJSONReportTemplate = class
+  private
+    FDoc : TJSONObject;
+    procedure AddNode(const aName : string; aValue : Integer); overload;
+    procedure AddNode(const aName, aValue : string); overload;
+  public
+    constructor Create();
+    destructor Destroy(); override;
+    procedure SetReportClass(const aReportClass : string);
+    procedure SetID(const aID : string);
+    procedure SetName(const aName : string);
+    procedure SetDay(aValue : Integer);
+    function AsString() : string;
+  end;
+
   TJsonReportsConfig = class(TInterfacedObject, IReportsConfig)
+  private
+    function ConfigDir : string;
+    function ConfigFile : string;
+    procedure CreateConfigDirectory;
+    procedure SaveJSONTemplateToFile(lJSON : TJSONReportTemplate);
   public
     procedure Add(aEntry : TReportConfig);
     function Entries() : ICollection<TReportConfig>;
@@ -15,6 +35,59 @@ type
 
 implementation
 
+{ TJSONReportTemplate }
+
+constructor TJSONReportTemplate.Create();
+begin
+  inherited;
+  FDoc := TJSONObject.Create();
+end;
+
+destructor TJSONReportTemplate.Destroy();
+begin
+  FDoc.Free();
+  inherited;
+end;
+
+procedure TJSONReportTemplate.SetDay(aValue : Integer);
+var
+  lCfg : TJSONObject;
+begin
+  lCfg := TJSONObject.Create();
+  lCfg.AddPair(TJSONPair.Create(TJSONString.Create('day'), TJSONNumber.Create(aValue)));
+  FDoc.AddPair(TJSONPair.Create(TJSONString.Create('config'), lCfg));
+end;
+
+procedure TJSONReportTemplate.SetID(const aID : string);
+begin
+  AddNode('id', aID);
+end;
+
+procedure TJSONReportTemplate.SetName(const aName : string);
+begin
+  AddNode('name', aName);
+end;
+
+procedure TJSONReportTemplate.SetReportClass(const aReportClass : string);
+begin
+  AddNode('class', aReportClass);
+end;
+
+procedure TJSONReportTemplate.AddNode(const aName : string; aValue : Integer);
+begin
+  FDoc.AddPair(TJSONPair.Create(TJSONString.Create(aName), TJSONNumber.Create(aValue)));
+end;
+
+procedure TJSONReportTemplate.AddNode(const aName, aValue : string);
+begin
+  FDoc.AddPair(TJSONPair.Create(TJSONString.Create(aName), TJSONString.Create(aValue)));
+end;
+
+function TJSONReportTemplate.AsString() : string;
+begin
+  Result := FDoc.ToString();
+end;
+
 { TJsonReportsConfig }
 
 procedure TJsonReportsConfig.Add(aEntry : TReportConfig);
@@ -22,41 +95,54 @@ begin
 
 end;
 
+function TJsonReportsConfig.ConfigDir() : string;
+begin
+  Result := TPath.GetHomePath() + '\Avocado\Timetable\';
+end;
+
+function TJsonReportsConfig.ConfigFile() : string;
+begin
+  Result := ConfigDir() + 'Config.json';
+end;
+
+procedure TJsonReportsConfig.CreateConfigDirectory;
+begin
+  TDirectory.CreateDirectory(ConfigDir());
+end;
+
 function TJsonReportsConfig.Entries() : ICollection<TReportConfig>;
 begin
 
 end;
 
+procedure TJsonReportsConfig.SaveJSONTemplateToFile(lJSON : TJSONReportTemplate);
+var
+  lList : TStringList;
+begin
+  CreateConfigDirectory();
+
+  lList := TStringList.Create();
+  try
+    lList.Add(lJSON.AsString());
+    lList.SaveToFile(ConfigFile());
+  finally
+    lList.Free();
+  end;
+end;
+
 procedure TJsonReportsConfig.SaveTemplate(const aTemplate : TReportTemplate);
 var
-  lFileName : string;
-  lList : TStringList;
-  lDir : string;
-  lDoc : TJSONObject;
-  lCfg : TJSONObject;
+  lJSON : TJSONReportTemplate;
 begin
-  lDir := TPath.GetHomePath() + '\Avocado\Timetable\';
-  TDirectory.CreateDirectory(lDir);
-  lFileName := lDir + 'Config.json';
-
-  lDoc := TJSONObject.Create();
+  lJSON := TJSONReportTemplate.Create();
   try
-    lDoc.AddPair(TJSONPair.Create(TJSONString.Create('class'), TJSONString.Create('MONTHLY')));
-    lDoc.AddPair(TJSONPair.Create(TJSONString.Create('id'), TJSONString.Create('VSA')));
-    lDoc.AddPair(TJSONPair.Create(TJSONString.Create('name'), TJSONString.Create('Valsts Sociâlâ Atskaite')));
-    lCfg := TJSONObject.Create();
-    lCfg.AddPair(TJSONPair.Create(TJSONString.Create('day'), TJSONNumber.Create(15)));
-    lDoc.AddPair(TJSONPair.Create(TJSONString.Create('config'), lCfg));
-
-    lList := TStringList.Create();
-    try
-      lList.Add(lDoc.ToString);
-      lList.SaveToFile(lFileName);
-    finally
-      lList.Free();
-    end;
+    lJSON.SetReportClass(aTemplate.ReportClass);
+    lJSON.SetID(aTemplate.ID);
+    lJSON.SetName(aTemplate.Name);
+    lJSON.SetDay(StrToInt(aTemplate.Config));
+    SaveJSONTemplateToFile(lJSON);
   finally
-    lDoc.Free();
+    lJSON.Free();
   end;
 end;
 
--- tst/integration/JsonReportsConfigTests.pas
+++ tst/integration/JsonReportsConfigTests.pas
@@ -29,20 +29,22 @@ var
   lFile : TStringList;
 const
   CONFIG_NAME = 'C:\Users\klaipand\AppData\Roaming\Avocado\Timetable\Config.json';
-
 begin
   lConfig := TJsonReportsConfig.Create();
+
+  lTemplate.ReportClass := 'MONTHLY';
   lTemplate.ID := 'VSA';
   lTemplate.Name := 'Valsts Sociâlâ Atskaite';
-  lTemplate.ReportClass := 'MONTHLY';
   lTemplate.Config := '15';
+
   lConfig.SaveTemplate(lTemplate);
 
   lFile := TStringList.Create();
   try
     lFile.LoadFromFile(CONFIG_NAME);
     Assert.AreEqual( //
-      '{"class":"MONTHLY","id":"VSA","name":"Valsts Sociâlâ Atskaite","config":{"day":15}}'#13#10, lFile.Text);
+      '{"class":"MONTHLY","id":"VSA","name":"Valsts Sociâlâ Atskaite","config":{"day":15}}'#13#10, //
+      lFile.Text);
   finally
     lFile.Free();
   end;

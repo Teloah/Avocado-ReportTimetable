2a265fd100c2d19eb0d1f07ab55d1f3a254c4656
Andris Klaipins <andris.klaipins@tieto.com>
2015-06-05 01:27:15 +0300

    integration test cleanup

--- src/model/ReportTemplate.pas
+++ src/model/ReportTemplate.pas
@@ -11,8 +11,19 @@ type
     Name : string;
     ReportClass : string;
     Config : string;
+    constructor Create(const aID : TReportID; const aReportClass, aName, aConfig : string);
   end;
 
 implementation
+
+{ TReportTemplate }
+
+constructor TReportTemplate.Create(const aID : TReportID; const aReportClass, aName, aConfig : string);
+begin
+  ID := aID;
+  ReportClass := aReportClass;
+  name := aName;
+  Config := aConfig;
+end;
 
 end.
--- tst/integration/JsonReportsConfigTests.pas
+++ tst/integration/JsonReportsConfigTests.pas
@@ -3,15 +3,20 @@ unit JsonReportsConfigTests;
 interface
 
 uses
-  System.Classes, DUnitX.TestFramework, ReportsConfig, JsonReportsConfig,
-  System.IOUtils;
+  System.Classes, System.IOUtils, DUnitX.TestFramework, ReportsConfig, JsonReportsConfig;
 
 type
 
   [TestFixture('Integration tests')]
   TJsonReportsConfigTests = class
   private
+    Config : IReportsConfig;
+    procedure AssertConfigIs(const aConfigString : string);
+    function CreateConfig() : IReportsConfig;
+    procedure RecreateConfigDir();
   public
+    [Setup]
+    procedure Setup();
     [Test]
     procedure SaveTemplate_OneTemplate_SavesIt();
   end;
@@ -21,38 +26,51 @@ implementation
 uses
   ReportTemplate;
 
-{ TJsonReportsConfigTests }
-
-procedure TJsonReportsConfigTests.SaveTemplate_OneTemplate_SavesIt();
-var
-  lConfig : IReportsConfig;
-  lTemplate : TReportTemplate;
-  lFile : TStringList;
 const
   CONFIG_DIR = '.\config';
   CONFIG_NAME = CONFIG_DIR + '\Config.json';
+
+  { TJsonReportsConfigTests }
+
+procedure TJsonReportsConfigTests.Setup();
 begin
-  TDirectory.Delete(CONFIG_DIR, True);
-  TDirectory.CreateDirectory(CONFIG_DIR);
+  RecreateConfigDir();
+end;
 
-  lConfig := TJsonReportsConfig.Create(CONFIG_DIR);
-
-  lTemplate.ReportClass := 'MONTHLY';
-  lTemplate.ID := 'VSA';
-  lTemplate.Name := 'Valsts Sociâlâ Atskaite';
-  lTemplate.Config := '15';
-
-  lConfig.SaveTemplate(lTemplate);
-
+procedure TJsonReportsConfigTests.AssertConfigIs(const aConfigString : string);
+var
+  lFile : TStringList;
+begin
   lFile := TStringList.Create();
   try
     lFile.LoadFromFile(CONFIG_NAME);
-    Assert.AreEqual( //
-      '{"class":"MONTHLY","id":"VSA","name":"Valsts Sociâlâ Atskaite","config":{"day":15}}'#13#10, //
-      lFile.Text);
+    Assert.AreEqual(aConfigString, lFile.Text);
   finally
     lFile.Free();
   end;
+end;
+
+function TJsonReportsConfigTests.CreateConfig() : IReportsConfig;
+begin
+  Result := TJsonReportsConfig.Create(CONFIG_DIR);
+end;
+
+procedure TJsonReportsConfigTests.RecreateConfigDir();
+begin
+  TDirectory.Delete(CONFIG_DIR, True);
+  TDirectory.CreateDirectory(CONFIG_DIR);
+end;
+
+procedure TJsonReportsConfigTests.SaveTemplate_OneTemplate_SavesIt();
+var
+  lTemplate : TReportTemplate;
+begin
+  lTemplate := TReportTemplate.Create('VSA', 'MONTHLY', 'Valsts Sociâlâ Atskaite', '15');
+  Config := CreateConfig();
+
+  Config.SaveTemplate(lTemplate);
+
+  AssertConfigIs('{"class":"MONTHLY","id":"VSA","name":"Valsts Sociâlâ Atskaite","config":{"day":15}}'#13#10);
 end;
 
 initialization

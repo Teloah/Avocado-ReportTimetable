9729199f25c9d4bfca0ed8691d31b38c98ea2e32
Andris Klaipins <andris.klaipins@tieto.com>
2016-02-15 20:57:05 +0200

    Added a complete tests suite for code coverage checking

---
+++ tst/CompleteTestSuite.dpr
@@ -0,0 +1,36 @@
+program CompleteTestSuite;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils,
+  TestInsight.DUnitX,
+  EndToEndTests in 'endtoend\EndToEndTests.pas',
+  JsonReportsConfigTests in 'integration\JsonReportsConfigTests.pas',
+  ReportTemplateTests in 'unit\ReportTemplateTests.pas',
+  ApplicationRunner in 'helpers\ApplicationRunner.pas',
+  ConfigHelper in 'helpers\ConfigHelper.pas',
+  NewReportFormRunner in 'helpers\NewReportFormRunner.pas',
+  JsonReportsConfig in '..\src\db\JsonReportsConfig.pas',
+  AddReport in '..\src\gui\AddReport.pas' {frmAddReport},
+  Main in '..\src\gui\Main.pas' {frmMain},
+  MainFormListener in '..\src\gui\MainFormListener.pas',
+  NewTemplate in '..\src\gui\NewTemplate.pas' {frmNewTemplate},
+  NewTemplateRequestListener in '..\src\gui\NewTemplateRequestListener.pas',
+  VCLTimetableApp in '..\src\gui\VCLTimetableApp.pas',
+  Reports in '..\src\model\Reports.pas',
+  ReportsConfig in '..\src\model\ReportsConfig.pas',
+  ReportTemplate in '..\src\model\ReportTemplate.pas',
+  TimetableApp in '..\src\TimetableApp.pas';
+
+begin
+  try
+    ReportMemoryLeaksOnShutdown := True;
+    RunRegisteredTests();
+  except
+    on E : Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+
+end.
